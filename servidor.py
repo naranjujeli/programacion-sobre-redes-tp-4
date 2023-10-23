5d8c9f2f12076e22482ec78d1cc7a0b433234308
naranjujeli <lucadive@gmail.com>
2023-10-23 20:50:40 -0300

    mejoramos

--- servidor.py
+++ servidor.py
@@ -1,33 +1,14 @@
-from aiohttp import web
-import socketio
+from flask import Flask
+from flask_socketio import SocketIO, send
 
 # Crear una instancia de Socket.IO
-sio = socketio.AsyncServer()
-app = web.Application()
-sio.attach(app)
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
-@sio.on("connection")
-async def connection():
-    print("SE PRENDIO ESTA MIERDA")
+@socketio.on("connection")
+async def connection(socket):
+    socket.send("hola", "hola")
+    
 
 if __name__ == '__main__':
-    web.run_app(app)
-
-# Definir una función que se ejecutará cuando un cliente se conecte
-# @sio.event
-# def connect(sid, environ):
-#     print(f"Cliente conectado: {sid}")
-
-# # Definir una función para recibir y enviar mensajes
-# @sio.event
-# def message(sid, data):
-#     print(f"Mensaje recibido de {sid}: {data}")
-#     # Puedes procesar el mensaje recibido y enviar una respuesta si es necesario
-#     response = {'message': 'Respuesta del servidor'}
-#     sio.emit('response', response, room=sid)
-
-# # Definir una función que se ejecutará cuando un cliente se desconecte
-# @sio.event
-# def disconnect(sid):
-#     print(f"Cliente desconectado: {sid}")
-
+    app.run(debug=True)
