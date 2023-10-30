84f692b8ef8dae63e8a7cd563dd750c249f90882
naranjujeli <lucadive@gmail.com>
2023-10-30 15:14:19 -0300

    Ya se conectan de nuevo

--- servidor.py
+++ servidor.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO
 from simulacion_web import SimulacionWeb
 from cronopio import ParserCronopio
 
@@ -8,32 +8,26 @@ from cronopio import ParserCronopio
 app = Flask(__name__)
 server = SocketIO(app, cors_allowed_origins='*')
 
-class Server:
+width = 750
+height = 400
+food = 500
+generation_size = 21
+frame = 15
+reproduction_pool_size = 7
+mutation_parameters = 0.4
 
-    def __init__(self): 
+parser = ParserCronopio()
+simulation = None
 
-        self.__width = 750
-        self.__height = 400
-        self.__food = 500
-        self.__generation_size = 21
-        self.__frame = 15
-        self.__reproduction_pool_size = 7
-        self.__mutation_parameters = 0.4
+@server.event
+async def connect(sock): 
+    socket = sock
+    simulation = SimulacionWeb(socket = socket, width=width, height=height, food=food, generation_size=generation_size, frame=frame, reproduction_pool_size=reproduction_pool_size, mutation_parameters=mutation_parameters)
 
-        self.__parser = ParserCronopio()
-
-
-
-    @server.event
-    def connect(self, sock): 
-        self.__socket = sock
-        self.__simulation = SimulacionWeb(socket = self.__socket, width=self.__width, height=self.__height, food=self.__food, generation_size=self.__generation_size, frame=self.__frame, reproduction_pool_size=self.__reproduction_pool_size, mutation_parameters=self.__mutation_parameters)
-        
-
-    @server.event
-    def dead_gen(self, data):
-        dead_cronopios = [self.__parser.from_json_to_cronopio(cronopio_json) for cronopio_json in data["cronopios"]]
-        self.__simulation.client.emit("new_gen", self.__simulation.generate(dead_cronopios))
+@server.event
+async def dead_gen(data):
+    dead_cronopios = [parser.from_json_to_cronopio(cronopio_json) for cronopio_json in data["cronopios"]]
+    simulation.client.emit("new_gen", simulation.generate(dead_cronopios))
 
 # @socketio.on("json")
 # def a(data):
